d1d681b194dd4fd8caabaec01754c158d873d333
Onyi <onyirhoda@gmail.com>
2025-10-07 17:45:57 -0400

    create some new directories

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -29,17 +29,19 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-mkdir data
+mkdir -p ./data
 
 
 # 2. Move the ./rawdata directory to ./data/raw
-mv rawdata data/raw
+mv ./rawdata ./data/raw
 
 
 # 3. List the contents of the ./data/raw directory
 ls ./data/raw
 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
+mkdir -p ./data/processed/server_logs ./data/processed/user_logs ./data/processed/event_logs
+
 
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
 
